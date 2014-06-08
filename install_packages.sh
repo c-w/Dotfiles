f2464b7650fd69fcfe5de2988128112dff2a5f22
c-w <clemens.wolff+git@gmail.com>
2014-06-08 02:04:08 +0100

    Simplify script (remove cross-platform wrappers)

--- install_packages.sh
+++ install_packages.sh
@@ -1,39 +1,20 @@
 #!/bin/sh
 
-set -o errexit
-set -o nounset
-
-fatal() { echo "$@" 1>&2 && exit 1; }
-exists() { command -v "$1" >/dev/null 2>&1; }
-
-install() {
-    local args="$@"
-    local distro="$(echo $(lsb_release -d | cut -d':' -f2))"
-
-    case "${distro}" in
-        Ubuntu*) installer="apt-get install" ;;
-        *)       fatal "unsupported distribution ${distro}" ;;
-    esac
-
-    sudo ${installer} ${args}
-}
-
-pypckg() { exists pip || install python-pip; sudo pip install "$@"; }
-
 # systems stuff
-install man
-install curl
+sudo apt-get install man
+sudo apt-get install curl
 
 # development stuff
-install build-essential
-install git
-install tmux
-install vim
-install cloc
-install ant
+sudo apt-get install build-essential
+sudo apt-get install git
+sudo apt-get install tmux
+sudo apt-get install vim
+sudo apt-get install cloc
+sudo apt-get install ant
 
 # python stuff
-install python
-install python3
-pypckg ipython
-pypckg virtualenv
+sudo apt-get install python
+sudo apt-get install python3
+sudo apt-get install python-pip
+sudo pip install ipython
+sudo pip install virtualenv
