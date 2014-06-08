35b4dea6141f6f423b6133efee9139afe6e1aaf3
c-w <clemens.wolff+git@gmail.com>
2014-06-08 02:09:20 +0100

    Add Oracle Java to installer

--- install_packages.sh
+++ install_packages.sh
@@ -3,6 +3,7 @@
 # systems stuff
 sudo apt-get install man
 sudo apt-get install curl
+sudo apt-get install software-properties-common python-software-properties
 
 # development stuff
 sudo apt-get install build-essential
@@ -10,6 +11,12 @@ sudo apt-get install git
 sudo apt-get install tmux
 sudo apt-get install vim
 sudo apt-get install cloc
+
+# java stuff
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java7-installer
+sudo apt-get install oracle-java7-set-default
 sudo apt-get install ant
 
 # python stuff
