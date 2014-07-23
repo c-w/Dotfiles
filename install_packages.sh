1c6b919f833d986d457c2929dacc1f4b92cf7b84
c-w <clemens.wolff+git@gmail.com>
2014-07-23 18:45:11 +0100

    Just install stuff instead of asking

--- install_packages.sh
+++ install_packages.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
 # systems stuff
-sudo apt-get install man
-sudo apt-get install curl
-sudo apt-get install software-properties-common python-software-properties
+sudo apt-get -y install man
+sudo apt-get -y install curl
+sudo apt-get -y install software-properties-common python-software-properties
 
 # development stuff
-sudo apt-get install build-essential
-sudo apt-get install git
-sudo apt-get install tmux
-sudo apt-get install vim
-sudo apt-get install cloc
+sudo apt-get -y install build-essential
+sudo apt-get -y install git
+sudo apt-get -y install tmux
+sudo apt-get -y install vim
+sudo apt-get -y install cloc
 
 # java stuff
-sudo add-apt-repository ppa:webupd8team/java && sudo apt-get update
-sudo apt-get install oracle-java7-installer oracle-java7-set-default
-sudo apt-get install ant
+sudo add-apt-repository -y ppa:webupd8team/java && sudo apt-get update
+sudo apt-get -y install oracle-java7-installer oracle-java7-set-default
+sudo apt-get -y install ant
 
 # python stuff
-sudo apt-get install python
-sudo apt-get install python3
-sudo apt-get install python-dev
-sudo apt-get install python-pip
+sudo apt-get -y install python
+sudo apt-get -y install python3
+sudo apt-get -y install python-dev
+sudo apt-get -y install python-pip
 sudo pip install ipython
 sudo pip install virtualenv
 sudo pip install flake8
