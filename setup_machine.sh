e5d4101ec39bf169c1454f3e38032107d393f855
Clemens Wolff <clewolff@microsoft.com>
2019-07-13 23:48:10 -0400

    Add docker setup

--- setup_machine.sh
+++ setup_machine.sh
@@ -71,6 +71,12 @@ main() {
     system_install python3-pip
     system_install python3-venv
 
+    # docker
+    curl -fsSL https://get.docker.com | sudo bash -
+    sudo usermod -aG docker "$USER"
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+
     # setup dot-files
     [ ! -d "${dotfiles_dir}" ] && git clone --recursive "${dotfiles_repo}" "${dotfiles_dir}"
     find "${dotfiles_dir}" \( \
