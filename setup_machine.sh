2c1608eab222bf5f246d78879bc091d613133f2c
Clemens Wolff <clewolff@microsoft.com>
2019-07-14 00:06:25 -0400

    Simplify script by assuming debian environment

--- setup_machine.sh
+++ setup_machine.sh
@@ -1,77 +1,37 @@
 #!/usr/bin/env bash
 
-################################################################################
-# constants
-################################################################################
-
 readonly dotfiles_dir="$HOME/Dotfiles"
 readonly dotfiles_repo='https://github.com/c-w/Dotfiles.git'
 readonly docker_compose_version='1.24.0'
 readonly nvm_version='v0.34.0'
 
-
-################################################################################
-# helper functions
-################################################################################
-
-error() {
-    local message="$1"
-    echo "${message}" >&2
-    exit 1
-}
-
-check_program_exists() {
-     local program="$1"
-     hash "${program}" 2>/dev/null
-}
-
-package_manager() {
-    local manager=''
-    $(check_program_exists 'apt-get') && manager='apt-get'
-    $(check_program_exists 'yum') && manager='yum'
-    [ -n "${manager}" ] \
-        && echo "${manager}" \
-        || error "no package manager found"
-}
-
-system_update() {
-    sudo "$(package_manager)" update
-    sudo "$(package_manager)" upgrade
-}
-
-system_install() {
-    local package="$1"
-    sudo "$(package_manager)" -y install "${package}"
-}
-
-
-################################################################################
-# main
-################################################################################
-
 main() {
-    system_update
+    sudo apt-get update
+    sudo apt-get upgrade -y
 
     # systems stuff
-    system_install man
-    system_install curl
-    system_install htop
-    system_install software-properties-common
-    system_install python-software-properties
-    system_install aspell-en
+    sudo apt-get install -y \
+        man \
+        curl \
+        htop \
+        software-properties-common \
+        python-software-properties \
+        aspell-en
 
     # development stuff
-    system_install build-essential
-    system_install git
-    system_install tmux
-    system_install vim
-    system_install cloc
+    sudo apt-get install -y \
+        build-essential \
+        git \
+        tmux \
+        vim \
+        cloc
 
     # python stuff
-    system_install python3
-    system_install python3-dev
-    system_install python3-pip
-    system_install python3-venv
+    sudo apt-get install -y \
+        python3 \
+        python3-dev \
+        python3-pip \
+        python3-venv
 
     # docker
     curl -fsSL https://get.docker.com | sudo bash -
